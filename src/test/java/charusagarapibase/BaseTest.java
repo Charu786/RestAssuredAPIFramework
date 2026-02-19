141a8ec06c9a46f0f98c0da8c0defe4d027091c8
sanja <charusagar35@gmail.com>
2026-02-19 23:59:52 +0530

    API Testing

--- src/test/java/charusagarapibase/BaseTest.java
+++ src/test/java/charusagarapibase/BaseTest.java
@@ -1,12 +1,37 @@
 package charusagarapibase;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.BeforeClass;
 
 public class BaseTest {
 
+    protected RequestSpecification requestSpec;
+    protected ResponseSpecification responseSpec;
+    protected ResponseSpecification createdSpec;
+
     @BeforeClass
     public void setup() {
-        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+
+        requestSpec = new RequestSpecBuilder()
+                .setBaseUri("https://jsonplaceholder.typicode.com")
+                .setContentType("application/json")
+                .build();
+
+        responseSpec = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .build();
+
+        createdSpec = new ResponseSpecBuilder()
+                .expectStatusCode(201)
+                .build();
+
+        RestAssured.requestSpecification = requestSpec;
     }
 }
+
+
